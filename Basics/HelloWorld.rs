7b853d0ba1d5f4fc37e3a5cac9ea832ae2f464aa
Rutuj Runwal <rutujrunwal001@gmail.com>
2023-12-31 12:58:14 +0530

    Feat: HellowWorld.rs

---
+++ Basics/HelloWorld.rs
@@ -0,0 +1,8 @@
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+    // Binary compilation: rustc HelloWorld.rs -> ./HelloWorld
+
+    // Print text to the console.
+    println!("Hello World!");
+}
