80d1eafdb9717f1d1c4ba224fb6157c6fe413d7e
Rutuj Runwal <rutujrunwal001@gmail.com>
2023-12-31 16:23:36 +0530

    Feat: Float Interpolation

---
+++ Basics/FloatsInterpolation.rs
@@ -0,0 +1,13 @@
+fn test(){
+    let x = 1.1;
+    let y = 2.2;
+
+    println!("x plus y: {} + {} = {}" ,x ,y ,x+y);
+    println!("x minus y: {} - {} = {}" ,x ,y ,x-y);
+    println!("x times y: {} * {} = {}" ,x ,y ,x*y);
+    println!("x by y: {} / {} = {}" ,x ,y ,x/y);
+}
+
+fn main(){
+    test();
+}
